21b3ef752f4ed399d7250b6e6e4da352f76b8945
venkat@777111 <vv952057@student.nitw.ac.in>
2024-07-15 17:51:03 +0530

    get pro update

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -105,7 +105,7 @@ export const createProduct = (productData, user) => async (dispatch) => {
       ));
     } else {
       ({ data } = await axios.post(
-        `https://new-backend-41vh.onrender.com/api/v1/seller/product/new`,
+        `/api/v1/seller/product/new`,
         productData,
         config
       ));
