581a20560f9a00b2df395c0eb232be241b0b02ba
venkat@777111 <vv952057@student.nitw.ac.in>
2024-07-15 18:19:11 +0530

    latest addition

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -67,6 +67,7 @@ export const register =
       dispatch({ type: REGISTER_USER_REQUEST });
 
       const config = {
+        withCredentials: true,
         headers: { "Content-Type": "multipart/form-data" },
       };
       // const server = process.env.REACT_APP_SERVER_URL;
